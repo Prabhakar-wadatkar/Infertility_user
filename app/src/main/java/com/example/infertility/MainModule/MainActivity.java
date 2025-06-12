4f954458e9226d27c4643812468030b47830b3a0
dnyanukogade <dnyaneshwarkogde1@gmail.com>
2025-06-12 13:59:13 +0530

    new screen added

--- app/src/main/java/com/example/infertility/MainModule/MainActivity.java
+++ app/src/main/java/com/example/infertility/MainModule/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.infertility.MainModule;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.infertility.UserAccountSettingsModule.AccountSettingsFragment;
 import com.example.infertility.R;
 import com.example.infertility.Utility;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,9 +18,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
         Utility.replaceFragment(getSupportFragmentManager(), R.id.container_for_home_screen, new HomeFragment(), false);
 
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if (id == R.id.home) {
+                    // Handle Home
+                    Utility.replaceFragment(getSupportFragmentManager(), R.id.container_for_home_screen, new HomeFragment(), true);
+                    return true;
+                } else if (id == R.id.accountSettings) {
+                    // Handle You
+                    Utility.replaceFragment(getSupportFragmentManager(), R.id.container_for_home_screen, new AccountSettingsFragment(), true);
+                    return true;
+                } else if (id == R.id.cart) {
+                    // Handle Cart
+                    return true;
+                } else if (id == R.id.diet) {
+                    // Handle Health Checkup
+                    return true;
+                } else if (id == R.id.feeling) {
+                    // Handle I'm Feeling
+                    return true;
+                }
+                return false;
+            }
+        });
+
+
+
+
     }
+
 }
---
+++ app/src/main/java/com/example/infertility/UserAccountSettingsModule/AccountSettingsFragment.java
@@ -0,0 +1,59 @@
+package com.example.infertility.UserAccountSettingsModule;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.infertility.Adapters.ViewPagerAdapter;
+import com.example.infertility.R;
+import com.google.android.material.tabs.TabLayout;
+
+public class AccountSettingsFragment extends Fragment {
+
+    public AccountSettingsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_account_settings, container, false);
+
+        TabLayout tabLayout = rootView.findViewById(R.id.tabLayout);
+        ViewPager2 viewPager = rootView.findViewById(R.id.viewPager);
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getActivity());
+        viewPager.setAdapter(viewPagerAdapter);
+        viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                tabLayout.getTabAt(position).select();
+            }
+        });
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+
+        return rootView;
+    }
+}
